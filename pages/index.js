f22c6b8e4a4131519cd4cb63c3ee3af371f23abc
auppal12 <auppal12@myseneca.ca>
2024-04-11 16:06:44 -0400

    optimized

--- pages/index.js
+++ pages/index.js
@@ -10,9 +10,14 @@
 ********************************************************************************/
 
 import { Row, Col } from "react-bootstrap";
-import Image from "react-bootstrap/Image";
+import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    document.title = "Art Explorer";
+  }, []);
+
   return (
     <>
       <h1 className="mt-4 mb-4 text-white lightfont" style={{ textAlign: 'center' }}>Welcome to the Art Gallery</h1>
@@ -20,15 +25,21 @@ export default function Home() {
       <hr />
       <br />
       <div className="text-white">
-        <Image fluid rounded
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Metropolitan_Museum_of_Art_-_panoramio_%284%29.jpg/2560px-Metropolitan_Museum_of_Art_-_panoramio_%284%29.jpg"
-          alt="Metropolitan Museum of Art"
-        />
+        <div style={{ textAlign: "center", overflow: "hidden", borderRadius: "10px" }}>
+          <Image
+            src="/Metropolitan_Museum_of_Art_-_panoramio.jpg"
+            alt="Metropolitan Museum of Art"
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{ width: '100%', height: 'auto' }}
+          />
+        </div>
         <hr />
         <Row className="mt-3">
           <Col md={6}>
             <p>
-              The Metropolitan Museum of Art, colloquially &quot;The Met,&quot; is located in Central Park, NYC.
+              The Metropolitan Museum of Art, colloquially &quot;The Met&quot;, is located in Central Park, NYC.
               It is one of the world&apos;s largest and finest art museums.
               The Met&apos;s mission is to collect, preserve, study, exhibit, and encourage appreciation
               for works that collectively represent the broad spectrum of human achievement and creativity.
@@ -38,7 +49,7 @@ export default function Home() {
               The firm of McKim, Mead &amp; White completed the wings on the Fifth Avenue facade in 1910. The modernistic glass sides and rear of the museum are the work of Roche-Dinkeloo.
             </p>
           </Col>
-          <Col md={6}>
+          <Col >
             <p>
               The Met Fifth Avenue measures almost 1⁄4-mile (400 m) long and with more than 2 million square feet (190,000 m2) of floor space, more than 20 times the size of the original 1880 building.
               The museum building is an accretion of over 20 structures, most of which are not visible from the exterior. The City of New York owns the museum building and contributes utilities, heat, and some of the cost of guardianship.
