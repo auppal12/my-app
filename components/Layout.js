13a287c53400f29af3c62965c117d154b367bccd
auppal12 <auppal12@myseneca.ca>
2024-04-11 01:39:30 -0400

    svg update

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import MainNav from './MainNav';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Layout(props) {
     return (
@@ -10,7 +9,6 @@ export default function Layout(props) {
             <br />
             <Container>
                 {props.children}
-                <SpeedInsights />
             </Container>
             <br />
         </>
